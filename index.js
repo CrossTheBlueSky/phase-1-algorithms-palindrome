faf9044a934ec4c0f3febf1690dcfc09af8fce8c
Derek Mercedes <mercedes.derek@gmail.com>
2023-11-01 11:34:39 -0600

    solves a palindrome with a string input and old array methods

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
+/*Logical Breakdown
+  Basic Palindrome Problem With Strings
+  1. First I'd convert the string to an array to get access toReversed()
+  2. return array === array.toReversed()
+  
+  Appended
+  1. toReversed doesn't seem to be implemented yet, so I'll use the old method
+  2. convert string to arr
+  3. Slice the full string and reverse into a new variable
+  4. convert new arr to string and compare to input
+*/
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const rev = word.split("").slice().reverse()
+  return word === rev.join("")
 }
 
 /* 
